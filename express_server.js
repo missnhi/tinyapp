224edfadd0f1285e4632e65c01bc8861a264ea3d
missnhi <nhi.phan.ley@gmail.com>
2024-10-11 13:18:56 -0600

    Update express_server.js to store short-url and long-url key value pair to database when a POST request is received to /urls Update express_server.js to respond with a redirect from /urls to /urls/:id

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const validUrl = require('valid-url');
 
+//Set the view engine to ejs
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -9,9 +11,12 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
-
+//Function to generate a random string for the shortURL: ex: b2xVn2
 function generateRandomString() {
+  return Math.random().toString(36).substr(2, 6);
 }
+
+console.log(generateRandomString());
 
 
 //POST requests body parsing middleware to make the POST body human readable.
@@ -19,8 +24,12 @@ app.use(express.urlencoded({extended: true}));
 
 //a POST Route to Receive the Form Submission
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("OkGood"); // Respond with 'Ok' (we will replace this)
+  // console.log(req.body); // Log the POST request body to the console
+  const id = generateRandomString();
+  urlDatabase[id] = req.body.longURL; // Add a new key-value pair to the urlDatabase object
+  // redirection to /urls/:id
+  console.log(urlDatabase);
+  res.redirect(`/urls/${id}`);
 });
 
 app.get("/", (req, res) => {
@@ -46,6 +55,20 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:id", (req, res) => {
   const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  
+  if (!longURL) {
+    return res.status(404).send("URL not found");
+  }
+// Validate the URL format to ensure it's a proper URL
+  if (!validUrl.isUri(longURL)) {
+    return res.status(400).send(`Invalid URL format`);
+  }
+  res.redirect(longURL);
+  
 });
 
 
