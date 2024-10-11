d8e75043eb2160ff72c17c5a3f739c79d9f077bc
missnhi <nhi.phan.ley@gmail.com>
2024-10-10 23:52:46 -0600

    Test the /urls/:id route

--- express_server.js
+++ express_server.js
@@ -26,7 +26,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls/:id", (req, res) => {
+  const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
+  res.render("urls_show", templateVars);
 });
+
+
 
