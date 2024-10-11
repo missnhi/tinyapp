ac6ce12d5058f850154e9624a9124c5e34317213
missnhi <nhi.phan.ley@gmail.com>
2024-10-11 12:47:38 -0600

    Add GET route to express_server.js and render using accompanying template Add POST route to express_server.js to receive form submission

--- express_server.js
+++ express_server.js
@@ -9,6 +9,20 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
+
+function generateRandomString() {
+}
+
+
+//POST requests body parsing middleware to make the POST body human readable.
+app.use(express.urlencoded({extended: true}));
+
+//a POST Route to Receive the Form Submission
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("OkGood"); // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -25,11 +39,15 @@ app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
 });
+
 
 
 
