92238f54962bb34894232ac910d3a8f6ca53e3f0
missnhi <nhi.phan.ley@gmail.com>
2024-10-13 01:15:47 -0600

    finish the login route

--- express_server.js
+++ express_server.js
@@ -121,7 +121,7 @@ app.post("/login", (req, res) => {
 //POST to/logout to clear the cookie
 app.post("/logout", (req, res) => {
   res.clearCookie('user_id');
-  res.redirect("/urls");
+  res.redirect("/login");
   
 });
 
